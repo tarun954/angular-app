074fd8d9fc47a0a7e3ddcfea98fd3f55ee71550c
tarun954 <tarunnaidu43637@gmail.com>
2022-05-16 23:08:09 +0530

    angular project

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {UiService} from '../../services/ui.service'
+import {Subscription} from 'rxjs'
+import {Router} from '@angular/router'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title: string = ' Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService:UiService, private router:Router) {
+    this.subscription = this.uiService.onToogle().subscribe((value) => (this.showAddTask = value));
+  }
+  ngOnInit(): void {}
+
+   toogleAddTask() {
+     this.uiService.toogleAddTask();
+   }
+   hasRoute(route: string) {
+     return this.router.url === route;
+   }
+
+}
---
+++ src/app/services/ui.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Observable, Subject} from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiService {
+  private showAddTask: boolean = false;
+  private subject = new Subject<any>();
+
+
+  constructor() { }
+
+toogleAddTask(): void {
+  this.showAddTask = !this.showAddTask;
+  this.subject.next(this.showAddTask)
+}
+
+onToogle(): Observable<any> {
+  return this.subject.asObservable();
+}
+}
